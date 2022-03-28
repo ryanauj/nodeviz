f4c233de7e443b898c5dc94c2ba06c5b61b2636d
Ryan Jackson <ryanauj@umich.edu>
2022-03-28 17:05:40 -0400

    Get mapping actually working.

--- web/src/index.js
+++ web/src/index.js
@@ -1,40 +1,48 @@
 const radius = 10
 
-function draw() {
-  var canvas = document.getElementById('main');
+function getContext() {
+  const canvas = document.getElementById('main')
   if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-    _draw(ctx)
+    return canvas.getContext('2d')
   }
+
+  throw 'No context available'
+}
+
+function draw() {
+  const ctx = getContext()
+  clear(ctx)
+  _draw(ctx)
+}
+
+function clear(ctx) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 }
 
 function _draw(ctx) {
-  ctx.beginPath()
-  const one = node(105, 55, 1)
-  const two = node(55, 75, 2)
+  const ax = Number(document.getElementById("ax").value)
+  const ay = Number(document.getElementById("ay").value)
+  const bx = Number(document.getElementById("bx").value)
+  const by = Number(document.getElementById("by").value)
 
-  connect(ctx, one, two)
+  console.log(ax)
+  console.log(ay)
+  console.log(bx)
+  console.log(by)
+  ctx.beginPath()
+  const one = node(ax, ay, 1)
+  const two = node(bx, by, 2)
+
   render(ctx, one)
   render(ctx, two)
+
+  connect(ctx, one, two)
 
   ctx.stroke()
 }
 
-function clearCircle(ctx, x, y) {
-  ctx.save()
-  ctx.beginPath()
-  ctx.arc(x, y, radius, 0, 2*Math.PI, true)
-  ctx.clip()
-  ctx.clearRect(x-radius, y-radius, radius*2, radius*2)
-  ctx.restore()
-}
-
 function node(x, y, num) {
   return { x, y, num }
-}
-
-function getAngle(a, b) {
-  return Math.atan((a.y - b.y) / (a.x - b.x))
 }
 
 function render(ctx, {x, y, num}) {
@@ -45,23 +53,30 @@ function render(ctx, {x, y, num}) {
   ctx.fillText(num, text.x, text.y)
 }
 
+function getAngle(a, b) {
+  return Math.atan((a.y - b.y) / (a.x - b.x))
+}
+
 function connect(ctx, a, b) {
-  const aToBAngle = getAngle(a, b)
-  const bToAAngle = getAngle(b, a)
-  const axMult = a.x < b.x ? 1 : -1
-  const ayMult = a.x < b.x ? 1 : -1
+  const angle = getAngle(a, b)
+
+  const aMult = a.x < b.x ? 1 : -1
   const edgeA = {
-    x: a.x + (axMult * (radius * Math.cos(aToBAngle))),
-    y: a.y + (ayMult * (radius * Math.sin(aToBAngle)))
+    mult: aMult,
+    x: a.x + (aMult * (radius * Math.cos(angle))),
+    y: a.y + (aMult * (radius * Math.sin(angle)))
   }
-  const bxMult = b.x < a.x ? 1 : -1
-  const byMult = b.x < a.x ? 1 : -1
+
+  const bMult = b.x < a.x ? 1 : -1
   const edgeB = {
-    x: b.x + (bxMult * (radius * Math.cos(bToAAngle))),
-    y: b.y + (byMult * (radius * Math.sin(bToAAngle)))
+    mult: bMult,
+    x: b.x + (bMult * (radius * Math.cos(angle))),
+    y: b.y + (bMult * (radius * Math.sin(angle)))
   }
-  console.log(edgeA)
-  console.log(edgeB)
+
+  console.log('a', edgeA)
+  console.log('b', edgeB)
+
   ctx.moveTo(edgeA.x, edgeA.y)
   ctx.lineTo(edgeB.x, edgeB.y)
 }
